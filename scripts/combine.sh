709c8d72e0e180d3152b849cc81a3f6c0c6ae947
Ionut Bizau <i@ibz.me>
2008-12-20 21:27:31 +0800

    Remove reference to print.css and add gzip for blueprint.

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 cat media/css/lib/jquery/*.css media/css/main.css > media/css/c/all.css
+cp media/css/lib/blueprint/screen.css media/css/c/blueprint-screen.css
+cp media/css/lib/blueprint/ie.css media/css/c/blueprint-ie.css
+
 cat media/javascript/lib/jquery/*.js media/javascript/utils.js | scripts/lib/jsmin.py > media/javascript/c/all.js
 
-gzip --to-stdout media/javascript/c/all.js > media/javascript/c/all.jgz
 gzip --to-stdout media/css/c/all.css > media/css/c/all.cgz
+gzip --to-stdout media/css/c/blueprint-screen.css > media/css/c/blueprint-screen.cgz
+gzip --to-stdout media/css/c/blueprint-ie.css > media/css/c/blueprint-ie.cgz
+
+gzip --to-stdout media/javascript/c/all.js > media/javascript/c/all.jgz
