75afc6981d8aa9854b6a264b8a55fc2d4bd21ae8
Ionut Bizau <i@ibz.me>
2008-12-20 20:32:48 +0800

    Add gzip compression to combine.sh.

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -2,3 +2,6 @@
 
 cat media/css/lib/jquery/*.css media/css/main.css > media/css/c/all.css
 cat media/javascript/lib/jquery/*.js media/javascript/utils.js | scripts/lib/jsmin.py > media/javascript/c/all.js
+
+gzip --to-stdout media/javascript/c/all.js > media/javascript/c/all.jgz
+gzip --to-stdout media/css/c/all.css > media/css/c/all.cgz
