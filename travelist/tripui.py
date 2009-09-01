3c06d8f4e7b29ab8ab90c4af1359df7103fbd062
Ionut Bizau <i@ibz.me>
2009-08-31 21:12:09 -0400

    Explain required date format in error message.

--- travelist/tripui.py
+++ travelist/tripui.py
@@ -5,8 +5,8 @@ from travelist import ui
 
 class EditForm(ui.ModelForm):
     name = forms.fields.CharField(widget=forms.widgets.TextInput(attrs={'class': 'title'}))
-    start_date = forms.fields.DateField(widget=forms.widgets.TextInput(attrs={'class': 'text'}))
-    end_date = forms.fields.DateField(widget=forms.widgets.TextInput(attrs={'class': 'text'}))
+    start_date = forms.fields.DateField(widget=forms.widgets.TextInput(attrs={'class': 'text'}), error_messages={'invalid': "Please enter the date as yyyy-mm-dd."})
+    end_date = forms.fields.DateField(widget=forms.widgets.TextInput(attrs={'class': 'text'}), error_messages={'invalid': "Please enter the date as yyyy-mm-dd."})
     class Meta:
         model = models.Trip
         fields = ('name', 'start_date', 'end_date', 'visibility')
